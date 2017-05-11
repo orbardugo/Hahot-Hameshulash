2e363aedf3c6b7da7f769296d81ab3b9c31ac4fc
Ruben <rubenazoulay90@gmail.com>
2017-05-11 15:42:47 +0300

    update person

--- QueryGenerator/Person.cs
+++ QueryGenerator/Person.cs
@@ -15,8 +15,19 @@ namespace QueryGenerator
         public int yearOfBirth { get; set; }
         public string city { get; set; }
         public string meetDate { get; set; }
+        public string CurrentOccupation { get; set; }
+        public string ExternalContact { get; set; }
+        public string UseAlcohol { get; set; }
+        public string UseDrug { get; set; }
+        public string Religion { get; set; }
+        public string CriminalRecord { get; set; }
+        Boolean[] Presence = new Boolean[366];
 
-        public Person(int yearofbirth, string firstname, string lastname, string gender, string city,string meetDate)
+
+
+        public Person(int yearofbirth, string firstname, string lastname, string gender, string city,
+         string meetDate, string currentOccupation, string externalContact, string useAlcohol, string useDrug,
+         string religion, string criminalRecord, Boolean[] presence)
         {
             yearOfBirth = yearofbirth;
             age = DateTime.Today.Year - yearofbirth;
@@ -24,8 +35,15 @@ namespace QueryGenerator
             lastName = lastname;
             this.city = city;
             this.gender = gender;
-            if(meetDate!="")
-                this.meetDate = meetDate.Substring(0,1);
+            if (meetDate != "")
+                this.meetDate = meetDate.Substring(0, 8);
+            CurrentOccupation = currentOccupation;
+            ExternalContact = externalContact;
+            UseAlcohol = useAlcohol;
+            UseDrug = useDrug;
+            Religion = religion;
+            CriminalRecord = criminalRecord;
+            Presence = presence;
         }
     }
 }
